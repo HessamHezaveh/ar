16618664d149a77d9b330a892275ee8b21a48ffd
HessamHezaveh <132199414+HessamHezaveh@users.noreply.github.com>
2023-12-14 12:07:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+console.log("THREE", THREE);
+import { MindARThree } from 'mindar-image-three'
+console.log(" MindARThree",  MindARThree);
+//import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//console.log("THREE", THREE);
